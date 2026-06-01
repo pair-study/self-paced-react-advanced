14684d937a4bffdda38a198f4d4b8920f0bb7b41
Nam Yooseong <nys8765@naver.com>
2026-06-01 22:41:07 +0900

    feat: App 컴포넌트 구조 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import Header from "./components/Header/Header.jsx";
+import CategoryFilter from "./components/CategoryFilter/CategoryFilter.jsx";
+import RestaurantList from "./components/RestaurantList/RestaurantList.jsx";
+import RestaurantDetailModal from "./components/RestaurantDetailModal/RestaurantDetailModal.jsx";
+import AddRestaurantModal from "./components/AddRestaurantModal/AddRestaurantModal.jsx";
 
 function App() {
-  return <h1>Self-Paced React</h1>;
+  return (
+    <>
+      <Header />
+      <main>
+        <CategoryFilter />
+        <RestaurantList />
+      </main>
+      <aside>
+        <RestaurantDetailModal />
+        <AddRestaurantModal />
+      </aside>
+    </>
+  );
 }
 
 export default App;
