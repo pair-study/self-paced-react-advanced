731955f546a710fa3a291077ca61da73d907d20c
Yeryeong Kang <dpfud8786@gmail.com>
2026-06-03 15:56:36 +0900

    feat: 템플릿 html -> 컴포넌트 분리 및 jsx 문법 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import Header from "./Header";
+import CategoryFilter from "./CategoryFilter";
+import RestaurantList from "./RestaurantList";
+import RestaurantDetailModal from "./RestaurantDetailModal";
+import AddRestaurantModal from "./AddRestaurantModal";
 
 function App() {
-  return <h1>Self-Paced React</h1>;
+  return (
+    <>
+      <Header />
+      <main>
+        <CategoryFilter />
+        <RestaurantList />
+      </main>
+      <aside>
+        <RestaurantDetailModal />
+        <AddRestaurantModal />
+      </aside>
+    </>
+  );
 }
 
 export default App;
