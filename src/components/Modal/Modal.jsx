e2cc78bdb9b8cb923060b635df35a18da10a9793
Nam Yooseong <nys8765@naver.com>
2026-06-07 23:00:30 +0900

    refactor: 불필요한 Modal 래퍼 제거

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,12 +2,12 @@ import styles from "./Modal.module.css";
 
 export default function Modal({ children, title, onClose }) {
   return (
-    <div className={`${styles.modal} ${styles["modal--open"]}`}>
+    <>
       <div className={styles.modal__backdrop} onClick={onClose}></div>
       <div className={styles.modal__container}>
         <h2 className={`${styles.modal__title} text-title`}>{title}</h2>
         {children}
       </div>
-    </div>
+    </>
   );
 }
