b0990da98476808b85b34b92412b47aadbfd7152
Yeryeong Kang <dpfud8786@gmail.com>
2026-06-08 17:43:12 +0900

    feat: 헤더 추가 버튼에 onClick prop 연결

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import styles from "./Header.module.css";
 import addButtonImg from "../../assets/add-button.png";
 
-export default function Header() {
+export default function Header({ onClick }) {
   return (
     <header className={styles.gnb}>
       <h1 className={`${styles.gnb__title} text-title`}>점심 뭐 먹지</h1>
@@ -9,6 +9,7 @@ export default function Header() {
         type="button"
         className={styles.gnb__button}
         aria-label="음식점 추가"
+        onClick={onClick}
       >
         <img src={addButtonImg} alt="음식점 추가" />
       </button>
