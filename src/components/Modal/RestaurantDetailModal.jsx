d37f2440969dc25423ea662b11bff702b24efaa2
Yeryeong Kang <dpfud8786@gmail.com>
2026-06-08 15:53:07 +0900

    feat: 모달에 클릭한 음식점 정보 표시

--- src/components/Modal/RestaurantDetailModal.jsx
+++ src/components/Modal/RestaurantDetailModal.jsx
@@ -1,20 +1,21 @@
 import styles from "./Modal.module.css";
 
-export default function RestaurantDetailModal() {
+export default function RestaurantDetailModal({ clickedRestaurant, onClose }) {
   return (
     <div className={`${styles.modal} ${styles["modal--open"]}`}>
-      <div className={styles.modal__backdrop}></div>
+      <div className={styles.modal__backdrop} onClick={onClose}></div>
       <div className={styles.modal__container}>
-        <h2 className={`${styles.modal__title} text-title`}>음식점 이름</h2>
+        <h2 className={`${styles.modal__title} text-title`}>
+          {clickedRestaurant.name}
+        </h2>
         <div className={styles["modal__restaurant-info"]}>
-          <p className="text-body">
-            음식점 소개 문구
-          </p>
+          <p className="text-body">{clickedRestaurant.description}</p>
         </div>
         {/* 닫기 버튼 */}
         <div className={styles["modal__button-container"]}>
           <button
             className={`${styles.button} ${styles["button--primary"]} text-caption`}
+            onClick={onClose}
           >
             닫기
           </button>
