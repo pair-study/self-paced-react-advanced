07dec6d500125d7e305559af8efa4b62ae1866bc
Yeryeong Kang <dpfud8786@gmail.com>
2026-06-08 18:12:48 +0900

    refactor: 재사용 가능한 Modal 컴포넌트 추출 및 children prop 적용

--- src/components/Modal/AddRestaurantModal.jsx
+++ src/components/Modal/AddRestaurantModal.jsx
@@ -2,6 +2,7 @@ import modalStyles from "./Modal.module.css";
 import formStyles from "./AddRestaurantModal.module.css";
 import { CATEGORIES } from "../../constants/categories";
 import { useState } from "react";
+import Modal from "./Modal";
 
 export default function AddRestaurantModal({ onSubmit, onClose }) {
   const [category, setCategory] = useState("");
@@ -14,86 +15,80 @@ export default function AddRestaurantModal({ onSubmit, onClose }) {
   };
 
   return (
-    <div className={`${modalStyles.modal} ${modalStyles["modal--open"]}`}>
-      <div className={modalStyles.modal__backdrop} onClick={onClose}></div>
-      <div className={modalStyles.modal__container}>
-        <h2 className={`${modalStyles.modal__title} text-title`}>
-          새로운 음식점
-        </h2>
-        <form onSubmit={handleSubmit}>
-          {/* 카테고리 */}
-          <div
-            className={`${formStyles["form-item"]} ${formStyles["form-item--required"]}`}
+    <Modal title="새로운 음식점" onClose={onClose}>
+      <form onSubmit={handleSubmit}>
+        {/* 카테고리 */}
+        <div
+          className={`${formStyles["form-item"]} ${formStyles["form-item--required"]}`}
+        >
+          <label htmlFor="category" className="text-caption">
+            카테고리
+          </label>
+          <select
+            name="category"
+            id="category"
+            value={category}
+            onChange={(e) => {
+              setCategory(e.target.value);
+            }}
+            required
           >
-            <label htmlFor="category" className="text-caption">
-              카테고리
-            </label>
-            <select
-              name="category"
-              id="category"
-              value={category}
-              onChange={(e) => {
-                setCategory(e.target.value);
-              }}
-              required
-            >
-              <option value="">선택해 주세요</option>
-              {CATEGORIES.map((category) => (
-                <option key={category} value={category}>
-                  {category}
-                </option>
-              ))}
-            </select>
-          </div>
-          {/* 음식점 이름 */}
-          <div
-            className={`${formStyles["form-item"]} ${formStyles["form-item--required"]}`}
+            <option value="">선택해 주세요</option>
+            {CATEGORIES.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+        {/* 음식점 이름 */}
+        <div
+          className={`${formStyles["form-item"]} ${formStyles["form-item--required"]}`}
+        >
+          <label htmlFor="name" className="text-caption">
+            이름
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            required
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        {/* 설명 */}
+        <div className={formStyles["form-item"]}>
+          <label htmlFor="description" className="text-caption">
+            설명
+          </label>
+          <textarea
+            name="description"
+            id="description"
+            cols="30"
+            rows="5"
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          ></textarea>
+          <span
+            className={`${formStyles["form-item__help-text"]} text-caption`}
           >
-            <label htmlFor="name" className="text-caption">
-              이름
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              required
-              value={name}
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-            />
-          </div>
-          {/* 설명 */}
-          <div className={formStyles["form-item"]}>
-            <label htmlFor="description" className="text-caption">
-              설명
-            </label>
-            <textarea
-              name="description"
-              id="description"
-              cols="30"
-              rows="5"
-              value={description}
-              onChange={(e) => {
-                setDescription(e.target.value);
-              }}
-            ></textarea>
-            <span
-              className={`${formStyles["form-item__help-text"]} text-caption`}
-            >
-              메뉴 등 추가 정보를 입력해 주세요.
-            </span>
-          </div>
-          {/* 추가 버튼 */}
-          <div className={modalStyles["modal__button-container"]}>
-            <button
-              className={`${modalStyles.button} ${modalStyles["button--primary"]} text-caption`}
-            >
-              추가하기
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+            메뉴 등 추가 정보를 입력해 주세요.
+          </span>
+        </div>
+        {/* 추가 버튼 */}
+        <div className={modalStyles["modal__button-container"]}>
+          <button
+            className={`${modalStyles.button} ${modalStyles["button--primary"]} text-caption`}
+          >
+            추가하기
+          </button>
+        </div>
+      </form>
+    </Modal>
   );
 }
--- src/components/Modal/RestaurantDetailModal.jsx
+++ src/components/Modal/RestaurantDetailModal.jsx
@@ -1,26 +1,21 @@
 import styles from "./Modal.module.css";
+import Modal from "./Modal";
 
 export default function RestaurantDetailModal({ clickedRestaurant, onClose }) {
   return (
-    <div className={`${styles.modal} ${styles["modal--open"]}`}>
-      <div className={styles.modal__backdrop} onClick={onClose}></div>
-      <div className={styles.modal__container}>
-        <h2 className={`${styles.modal__title} text-title`}>
-          {clickedRestaurant.name}
-        </h2>
-        <div className={styles["modal__restaurant-info"]}>
-          <p className="text-body">{clickedRestaurant.description}</p>
-        </div>
-        {/* 닫기 버튼 */}
-        <div className={styles["modal__button-container"]}>
-          <button
-            className={`${styles.button} ${styles["button--primary"]} text-caption`}
-            onClick={onClose}
-          >
-            닫기
-          </button>
-        </div>
+    <Modal title={clickedRestaurant.name} onClose={onClose}>
+      <div className={styles["modal__restaurant-info"]}>
+        <p className="text-body">{clickedRestaurant.description}</p>
       </div>
-    </div>
+      {/* 닫기 버튼 */}
+      <div className={styles["modal__button-container"]}>
+        <button
+          className={`${styles.button} ${styles["button--primary"]} text-caption`}
+          onClick={onClose}
+        >
+          닫기
+        </button>
+      </div>
+    </Modal>
   );
 }
