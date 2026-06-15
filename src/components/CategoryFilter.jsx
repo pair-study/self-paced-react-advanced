f6b62131f79990fffebabfff0073268cdf200acf
Nam Yooseong <nys8765@naver.com>
2026-06-16 01:04:08 +0900

    refactor: CategoryFilter 컴포넌트 styled-components 전환

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,13 +1,29 @@
-import { ALL_CATEGORY, CATEGORIES } from "../../constants/categories.js";
-import styles from "./CategoryFilter.module.css";
+import { ALL_CATEGORY, CATEGORIES } from "../constants/categories.js";
+import styled from "styled-components";
+
+const CategoryFilterSection = styled.section`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 16px;
+  margin-top: 24px;
+
+  select {
+    height: 44px;
+    min-width: 125px;
+    border: 1px solid #d0d5dd;
+    border-radius: 8px;
+    background: transparent;
+    font-size: 16px;
+    padding: 8px;
+  }
+`;
 
 export default function CategoryFilter({ category, onCategoryChange }) {
   return (
-    <section className={styles.categoryFilter}>
+    <CategoryFilterSection>
       <select
         name="category"
         id="category-filter"
-        className={styles.categoryFilter__select}
         aria-label="음식점 카테고리 필터"
         value={category}
         onChange={onCategoryChange}
@@ -19,6 +35,6 @@ export default function CategoryFilter({ category, onCategoryChange }) {
           </option>
         ))}
       </select>
-    </section>
+    </CategoryFilterSection>
   );
 }
