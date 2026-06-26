7080bccb6e9e573a98bf58516dcc952cda520383
Yeryeong Kang <dpfud8786@gmail.com>
2026-06-27 05:10:42 +0900

    refactor: Provider를 main.jsx로 이동하고 AppContent 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import "./App.css";
-import { RestaurantProvider } from "./context/RestaurantContext";
-import { ModalProvider } from "./context/ModalContext";
-import AppContent from "./components/AppContent";
+import Header from "./components/Header/Header";
+import CategoryFilter from "./components/CategoryFilter/CategoryFilter";
+import RestaurantList from "./components/RestaurantList/RestaurantList";
+import RestaurantDetailModal from "./components/Modal/RestaurantDetailModal";
+import AddRestaurantModal from "./components/Modal/AddRestaurantModal";
+import { useRestaurantContext } from "./context/useRestaurantContext";
+import { useModalContext } from "./context/useModalContext";
 
-function App() {
+export default function App() {
+  const { error, isLoading } = useRestaurantContext();
+  const { clickedRestaurant, isAddModalOpen, handleAddModalOpen } = useModalContext();
+
   return (
-    <RestaurantProvider>
-      <ModalProvider>
-        <AppContent />
-      </ModalProvider>
-    </RestaurantProvider>
+    <>
+      <Header onClick={handleAddModalOpen} />
+      <main>
+        {isLoading && <p>로딩중입니다.</p>}
+        {error && <p>{error}</p>}
+        <CategoryFilter />
+        <RestaurantList />
+      </main>
+      <aside>
+        {clickedRestaurant && <RestaurantDetailModal />}
+        {isAddModalOpen && <AddRestaurantModal />}
+      </aside>
+    </>
   );
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { RestaurantProvider } from "./context/RestaurantContext";
+import { ModalProvider } from "./context/ModalContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RestaurantProvider>
+      <ModalProvider>
+        <App />
+      </ModalProvider>
+    </RestaurantProvider>
   </React.StrictMode>
 );
