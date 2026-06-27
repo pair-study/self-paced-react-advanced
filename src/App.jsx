061f77ff70e8ab8a81caf86d6d6dee358d2c4f9d
Nam Yooseong <nys8765@naver.com>
2026-06-28 02:33:18 +0900

    refactor: App에서 selectedCategory를 스토어로 교체

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,15 @@ import RestaurantList from "./components/RestaurantList.jsx";
 import { useState } from "react";
 import RestaurantDetailModal from "./components/RestaurantDetailModal.jsx";
 import AddRestaurantModal from "./components/AddRestaurantModal.jsx";
-import { ALL_CATEGORY } from "./constants/categories.js";
+import useRestaurantStore from "./store/useRestaurantStore.js";
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState(ALL_CATEGORY);
+  const selectedCategory = useRestaurantStore(
+    (state) => state.selectedCategory,
+  );
+  const setSelectedCategory = useRestaurantStore(
+    (state) => state.setSelectedCategory,
+  );
   const [clickedRestaurant, setClickedRestaurant] = useState(null);
   const [isAddRestaurantModalOpen, setIsAddRestaurantModalOpen] =
     useState(false);
